d5a889ba18dd48fb83640050ffad800b5ac4cb89
Tanmay-r <tvrandhavane@gmail.com>
2014-02-12 01:14:12 +0530

    Read configuration file

--- src/broker.cpp
+++ src/broker.cpp
@@ -1,38 +1,94 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <queue>
 
-int main ()
+using namespace std;
+
+struct Node
 {
-  	//Reading config file
+    string ip_addr;
+    string port_number;
+    int max_house_processes;
+    int remaining_house_processes;
+    Node(string ip, string port, int max): ip_addr(ip), port_number(port), max_house_processes(max), remaining_house_processes(max)
+    {
+    }
+};
 
+class CompareNode
+{
+    bool reverse;
+
+    public:
+    CompareNode(const bool& rev=false){ reverse=rev;}
+    bool operator()(const Node * lhs, const Node * rhs) const
+    {
+        if(reverse) return (lhs->remaining_house_processes > rhs->remaining_house_processes);
+        else return (lhs->remaining_house_processes < rhs->remaining_house_processes);
+    }
+};
+
+int readConfigFile(priority_queue<Node*, vector<Node* >, CompareNode> *node_info)
+{
+    //Reading config file
+    ifstream file("priv/nodes.config");
+    string buffer;
+    string ip_addr;
+    string port_number;
+    int max_house_processes;
+
+    while (getline(file, buffer))
+    {
+        unsigned pos_colon = buffer.find_first_of(":");
+        ip_addr = buffer.substr(0, pos_colon - 1);
+        unsigned pos_comma = buffer.find_first_of(",");
+        port_number = buffer.substr(pos_colon + 1, pos_comma - pos_colon - 1);
+        max_house_processes = atoi(buffer.substr(pos_comma + 1).c_str());
+        Node *newNode = new Node(ip_addr, port_number,max_house_processes);
+        node_info->push(newNode);
+    }
+    return 1;
+}
+
+int main()
+{
+    //Read config file
+    priority_queue<Node*, vector<Node* >, CompareNode> *node_info = new priority_queue<Node*, vector<Node* >, CompareNode>();
+    readConfigFile(node_info);
+
+    while(!node_info->empty())
+    {
+        Node * n = node_info->top();
+        node_info->pop();
+        cout << n->ip_addr << " " << n->port_number << " "<< n->max_house_processes << endl;
+    }
 
     //Read the data stream
-    std::ifstream file("priv/temp.csv");
-    std::string buffer;
+    ifstream file("priv/temp.csv");
+    string buffer;
     int house_id;
 
     /*unordered_map <int, pid_t> m;
     for(int i = 0; i < SIZE; ++i)
     {
-        m[std::to_string(i)] = i;
+        m[to_string(i)] = i;
     }
     for(int i = 0; i < (SIZE-1); ++i)
     {
-        m[std::to_string(i)] = m[std::to_string(i+1)];
+        m[to_string(i)] = m[to_string(i+1)];
     }*/
 
-    while (std::getline(file, buffer))
+    while (getline(file, buffer))
     {
-    	std::string message;
+    	string message;
     	unsigned pos_first = buffer.find_first_of(","); //need to remove sample id
     	unsigned pos_last = buffer.find_last_of(","); //need to get the starting point of the house id
     	message = buffer.substr(pos_first + 1, pos_last - pos_first - 1);
     	house_id = atof(buffer.substr(pos_last + 1).c_str());	//house id string to int
-    	std::cout << "house_id = " << house_id << std::endl;
-        std::cout << "message = " << message << std::endl;
+    	//cout << "house_id = " << house_id << endl;
+        //cout << "message = " << message << endl;
     	//send message to the house with house id
-
 
     }
 	return 0;
