9e1b10f6245952e9a2cbe0fde92a3f5a3b8ac2ee
Aman Mangal <mangalaman93@gmail.com>
2014-02-22 13:08:20 +0530

    copied from Erlang code, testing not done

--- include/slidingmc.h
+++ include/slidingmc.h
@@ -9,14 +9,33 @@ using namespace std;
 #define WINDOW_SIZE 24*3600
 #define NUM_BINS 100
 
+struct bin
+{
+	float val;
+	int freq;
+
+	bin(float v=-1, int f=0)
+	: val(v), freq(f) {}
+};
+
 class SlidingMc
 {
 	int size;
-	int array[NUM_BINS];
+	int num_bins;
+	bin *bins;
+	Queue<float> *queue;
+	unsigned base_ts;
+
+	float findMedian(int index, int cum, int mindex);
+	int binarySearch(int first, int last, float val);
+	void addNewBin(int pos, bin b);
 
   public:
-	SlidingMc();
+	SlidingMc(unsigned int ts);
 	~SlidingMc();
-	float getMedian();
-	void insert(float elem);
+	void init(unsigned int ts, int size);
+	float getMedian(unsigned int ts);
+	void insert(unsigned int ts, float val);
 };
+
+#endif
