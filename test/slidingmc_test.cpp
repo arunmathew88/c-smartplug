d1967094b56005d941a03ce84e01d7956378e667
Aman Mangal <mangalaman93@gmail.com>
2014-02-23 18:00:29 +0530

    overlap window tested

--- test/slidingmc_test.cpp
+++ test/slidingmc_test.cpp
@@ -76,8 +76,23 @@ class SlidingMcTest : public CppUnit::TestFixture
 		CPPUNIT_ASSERT(sm->getMedian(10, WINDOW_24HR) == 1);
 		CPPUNIT_ASSERT(sm->getMedian(1000, WINDOW_24HR) == 1);
 		CPPUNIT_ASSERT(sm->getMedian(10000, WINDOW_24HR) == 1);
-		cout<<sm->getMedian(100000, WINDOW_24HR)<<endl;
-		CPPUNIT_ASSERT(sm->getMedian(100000, WINDOW_24HR) == 1);
+		CPPUNIT_ASSERT(sm->getMedian(100000, WINDOW_24HR) == -1);
+	}
+
+	void test_overlap1()
+	{
+		for(int i=1; i<24*3600+1; i++)
+			sm->insert(i, i);
+		sm->insert(MAX_WINDOW_SIZE+100, 100);
+		CPPUNIT_ASSERT(sm->getMedian(MAX_WINDOW_SIZE+100, WINDOW_24HR) == 48051.5);
+	}
+
+	void test_overlap2()
+	{
+		for(int i=1; i<24*3600+1; i++)
+			sm->insert(i, 24*3600-i);
+		sm->insert(MAX_WINDOW_SIZE+100, 100);
+		CPPUNIT_ASSERT(sm->getMedian(MAX_WINDOW_SIZE+100, WINDOW_24HR) == 38352);
 	}
 
 	CPPUNIT_TEST_SUITE(SlidingMcTest);
@@ -86,6 +101,9 @@ class SlidingMcTest : public CppUnit::TestFixture
 	CPPUNIT_TEST(test3);
 	CPPUNIT_TEST(test4);
 	CPPUNIT_TEST(test5);
+	CPPUNIT_TEST(test6);
+	CPPUNIT_TEST(test_overlap1);
+	CPPUNIT_TEST(test_overlap2);
 	CPPUNIT_TEST_SUITE_END();
 };
 
